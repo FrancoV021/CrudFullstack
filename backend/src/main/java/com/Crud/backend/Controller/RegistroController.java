98a5cd57dc6ae29fb16b84fb233d32aefaa182ad
Franco Santacatalina. <Franhi02@hotmail.com>
2025-10-05 16:39:31 -0300

    Primer commit del proyecto CrudFullstack - guardado de la priemra parte del backend terminado, listo para ser testeado por postman o curl

--- backend/src/main/java/com/Crud/backend/Controller/RegistroController.java
+++ backend/src/main/java/com/Crud/backend/Controller/RegistroController.java
@@ -2,7 +2,8 @@ package com.Crud.backend.Controller;
 
 import com.Crud.backend.Dtos.RegistroDto;
 import com.Crud.backend.Service.RegistroService;
-import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 // Clase CONTROLLER - es la encargada de procesar las peticiones del usuario y determinar la acción a seguir para realizar las operaciones CRUD.
 @RestController
-@RequiredArgsConstructor
 public class RegistroController {
+    @Autowired
     private RegistroService registroService;
 
     @GetMapping("/gym/registros")
